9b63289a05bb6b6799c2bdcea32d565f5aa448b4
ZFlyRay <cuitzfr@hotmail.com>
2018-05-10 11:13:19 +0800

    init code and files

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App';
+import fastClick from 'fastclick';
+import router from './router';
+import './assets/style/css/reset.css';
+
+Vue.config.productionTip = false;
+fastClick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
-})
+});
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    home
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'Home'
+  };
+</script>
+
+<style scoped lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+
+</style>
