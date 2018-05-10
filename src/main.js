d9ef561ab29eb9ddcf5ac9f9b231c614d8e9260d
ZFlyRay <cuitzfr@hotmail.com>
2018-05-10 16:18:32 +0800

    swiper

--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ import Vue from 'vue';
 import App from './App';
 import fastClick from 'fastclick';
 import router from './router';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+
 import 'style/css/reset.css';
+import 'style/css/border.css';
 import 'style/css/iconfont.css';
+import 'swiper/dist/css/swiper.css';
 
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper);
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'HomeSwiper',
+    data() {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          loop: true
+        },
+        swiperList: [
+          {
+            id: '0001',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/c4/1cdd28811593b802.jpg_750x200_5fbb7c91.jpg'
+          },
+          {
+            id: '0002',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/5c/969bfa7af00a2702.jpg_750x200_b99b8e07.jpg'
+          }
+        ]
+      };
+    }
+  };
+</script>
+
+<style scoped lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+  .wrapper
+    width: 100%
+    height: 0
+    padding-bottom: 26.67%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
